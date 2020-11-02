365ad3c3668b5f590104d7e6ab99b3e27ee8bc2f
javier salas <javier.salas.991@gmail.com>
2020-11-02 20:05:32 -0300

    prueba whatsapp

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -5,6 +5,7 @@ const Inicio = () => {
     return (
         <div>
             <Carrusel2></Carrusel2>
+            <a href="https://wa.me/543813592337">whatsapp prueba</a>
         </div>
     );
 };
