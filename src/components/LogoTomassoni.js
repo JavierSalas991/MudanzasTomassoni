70d93fd288bc29e77a920f9a50967089831023cc
javier salas <javier.salas.991@gmail.com>
2020-11-04 20:08:46 -0300

    guardo

--- src/components/LogoTomassoni.js
+++ src/components/LogoTomassoni.js
@@ -5,7 +5,7 @@ const LogoTomassoni = () => {
     return (
         <div className="d-none d-md-block">
             <div className="d-flex justify-content-center">
-                <img src={LogoEmpresa} alt="" className="imgLogo"/>
+                <img src={LogoEmpresa} alt="" className="imgLogo mt-2"/>
             </div>
         </div>
 
